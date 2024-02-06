db0a9e1356020d2476098e3e8767bbc8582ebd0d
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-06 20:46:01 +0530

    getUsersForSidebar functionality added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
 dotenv.config();
 
 const app = express();
@@ -20,6 +22,7 @@ app.get("/", (req, res)=>{
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 
 app.listen(PORT, () => {

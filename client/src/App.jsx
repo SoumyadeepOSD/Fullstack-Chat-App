5fd5047811e53e8dca194392ed8cc61507a2f3ba
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-07 22:36:37 +0530

    Connection between frontend and backend successfully established

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,18 @@ import Login from './pages/login/Login'
 import './index.css'
 import Signup from './pages/signup/Signup'
 import Home from './pages/home/Home'
+import {Route, Routes} from 'react-router-dom';
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <Signup /> */}
-      <Home/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+        </Routes>
+        <Toaster/>
     </div>
   )
 }
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
     <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -20,9 +22,9 @@ const Login = () => {
           <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10" />
         </div>
 
-        <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+        <Link to={"/signup"} className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
           {"Don't"} have an account?
-        </a>
+        </Link>
 
         <div className="flex justify-center mt-5">
           <button className="btn btn-outline btn-primary">Login</button>

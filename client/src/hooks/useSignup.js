e42d2cc6ade64bb025f9ab67493de84235d6dfdf
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-08 08:19:10 +0530

    Signup and logout functionality added

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const {setAuthUser} = useAuthContext();
   const signup = async({fullName, userName, password, confirmPassword, gender}) => {
     const success = handleInputErrors({fullName, userName, password, confirmPassword, gender});
     if(!success) return;
@@ -19,7 +21,13 @@ const useSignup = () => {
         });
 
         const data = await res.json();
-        console.log(data);
+        if(data.error){
+            throw new Error(data.error);
+        }
+        // Localstorage
+        localStorage.setItem("chat-user", JSON.stringify(data));
+        // Context value
+        setAuthUser(data);
     } catch (error) {
         toast.error(error.message);
     } finally{

215ee1fc4eb982b9507b50ad3dfc2bbf3d19d461
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-14 11:53:35 +0530

    Individual chat access functionality with chat sending to database completed

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -1,18 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect } from 'react'
 import Messages from './Messages';
 import MessageInput from './MessageInput';
 import {TiMessages} from 'react-icons/ti';
+import useConversation from '../../zustand/useConversation';
 
 const MessageContainer = () => {
-    const noChatSelected = false;
-  return (
+    const {selectedConversation,setSelectedConversation} = useConversation();
+    
+    // *This useEffect is used to clean the state of the chatsection
+    useEffect(()=>{
+        return() => setSelectedConversation(null);
+    },[setSelectedConversation]);
+  
+    return (
     <div className='md:min-w-[450px] flex flex-col'>
-        {noChatSelected ? (
+        {!selectedConversation ? (
             <NoChatSelected/>
         ):(<>
             <div className='bg-slate-500 px-4 py-2 mb-2'>
-                <span className='label-text'>To:</span><span className='text-gray-900 font-bold'>John Doe</span>
+                <span className='label-text'>To:</span>{" "}
+                <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
             </div>
             <Messages/>
             <MessageInput/>
--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -1,27 +1,35 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import useConversation from '../../zustand/useConversation';
 
-const Conversation = () => {
-  return (
-    <div>
-        <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
-            <div className='avatar online'>
-                <div className='w-12 rounded-full'>
-                    <img src="https://static.vecteezy.com/system/resources/previews/006/487/917/original/man-avatar-icon-free-vector.jpg" alt="user avatar" />
+const Conversation = ({ conversation, emoji, lastIdx }) => {
+    const { selectedConversation, setSelectedConversation } = useConversation();
+    const isSelected = selectedConversation?._id === conversation._id;
+    return (
+        <div>
+            <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+                ${isSelected ? "bg-sky-500" : ""}`}
+                onClick={() => setSelectedConversation(conversation)}>
+                <div className='avatar online'>
+                    <div className='w-12 rounded-full'>
+                        <img
+                            src={conversation.profilePic}
+                            alt="user avatar"
+                        />
+                    </div>
+                </div>
+
+                <div className='flex flex-col flex-1'>
+                    <div className='flex gap-3 justify-between'>
+                        <p className='font-bold'>{conversation.fullName}</p>
+                        <span className='text-xl'>{emoji}</span>
+                    </div>
                 </div>
             </div>
 
-            <div className='flex flex-col flex-1'>
-                <div className='flex gap-3 justify-between'>
-                    <p className='font-bold'>John Doe</p>
-                    <span className='text-xl'>🎃</span>
-                </div>
-            </div>
+            {!lastIdx && <div className='divider my-0 py-0 h-1' />}
         </div>
-
-        <div className='divider my-0 py-0 h-1'/>
-    </div>
-  )
+    )
 }
 
 export default Conversation;

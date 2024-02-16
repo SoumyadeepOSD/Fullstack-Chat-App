59ed939e5b7fe869b17026ea7568bbd75877e2a7
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-16 21:36:59 +0530

    minor bug fixed

--- server/server.js
+++ server/server.js
@@ -1,29 +1,33 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import connectToMongoDB from './db/connectToMongoDB.js';
+
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
+
+import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
+
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
-const PORT = process.env.PORT ;
-
 app.use(express.json());
 app.use(cookieParser());
-
-
-// *Root Route
-app.get("/", (req, res)=>{
-    res.send("Hello World");
-});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist" ,"index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();

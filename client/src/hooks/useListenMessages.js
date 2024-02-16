bd2454eaf91783dbcaa4b5b30fa2164af9df7974
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-16 20:33:38 +0530

    Project completed

--- client/src/hooks/useListenMessages.js
+++ client/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext"
 import useConversation from "../zustand/useConversation";
+import notsound from "../assets/sounds/notsound.mp3";
 
 const useListenMessages = () => {
     const {socket} = useSocketContext();
@@ -8,9 +9,12 @@ const useListenMessages = () => {
 
     useEffect(()=>{
         socket?.on("newMessage", (newMessage)=>{
+            newMessage.shouldShake = true;
+            const sound = new Audio(notsound);
+            sound.play();
             setMessages([...messages, newMessage]);
         })
-
+        // *Not listening the event more than 1 time
         return () => socket?.off("newMessage");
     },[socket, setMessages, messages]);
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,16 @@ export default defineConfig({
       "/api":{
         target: "http://localhost:8000",
       },
+    },
+    fs: {
+      // Allow serving files from 'assets' directory
+      allow: ['..']
+    }
+  },
+  resolve: {
+    alias: {
+      // Setting up an alias for accessing assets easily in your code
+      '@assets': path.resolve(new URL('.', import.meta.url).pathname, './assets')
     }
   }
 })

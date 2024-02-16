debaf70480adfbe3d0700b9d6303dd1e74f3dd10
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-16 20:04:13 +0530

    Realtime chat with socket.io is implemented successfully

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -9,10 +9,11 @@ const Message = ({message}) => {
     const {selectedConversation} = useConversation();
     const fromMe = message.senderId === authUser._id;
     const formattedTime = extractTime(message.createdAt);
-    const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+    const chatClassName = fromMe ? "chat-end" : "chat-start";
     const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic
     const bubbleBgColor = fromMe ? 'bg-blue-500' : '';
 
+    console.log("message", message);
 
 
     return (
@@ -22,7 +23,7 @@ const Message = ({message}) => {
                     <img src={profilePic} alt="user avatar" />
                 </div>
             </div>
-            <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgColor} pb-2`}>{message.message}</div>
+            <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
             <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
         </div>
     )
--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
+import { app, server } from './socket/socket.js';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT ;
 
 app.use(express.json());
@@ -25,7 +25,7 @@ app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
